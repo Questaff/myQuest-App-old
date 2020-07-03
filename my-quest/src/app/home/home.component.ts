b7c038b0cdba8184b640cbc5fb882c10d5a847bc
lefrem <remy.lefrancois.pro@gmail.com>
2020-07-03 02:46:06 +0200

    Développement du composant "Home"

--- my-quest/src/app/home/home.component.ts
+++ my-quest/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router,
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
+  redirectionRegister() {
+    this.router.navigate(['register']);
+  }
+
+  redirectionLogin() {
+    this.router.navigate(['login']);
+  }
 }
