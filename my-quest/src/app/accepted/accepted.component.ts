a0034dc3d43250fc8b06e0e58b897cb8a131461a
lefrem <remy.lefrancois.pro@gmail.com>
2020-07-03 02:29:10 +0200

    Création des composants et des services

---
+++ my-quest/src/app/accepted/accepted.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accepted',
+  templateUrl: './accepted.component.html',
+  styleUrls: ['./accepted.component.scss'],
+})
+export class AcceptedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
